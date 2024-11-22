47e996c029f4130f218ecfc3b9b813c8ab565ca5
asahina820 <hina.gntm@gmail.com>
2024-11-22 10:51:05 +0900

    feat: set configuration file and change name

--- src/extensions/iss_tracker/iss_tracker.ts
+++ src/extensions/iss_tracker/iss_tracker.ts
@@ -1,4 +1,4 @@
-import html from "@distui/demo/main/index.html?raw";
+import html from "@distui/iss_tracker/main/index.html?raw";
 
 import { GlobalThis, MouseEventProps } from "@/shared/reearthTypes";
 
